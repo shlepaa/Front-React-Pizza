66c301a51712e412099425ec63648959273d89a6
Igor Raskatov <aura.star@yandex.ru>
2022-12-18 23:41:08 +0300

    Add thunk action for fetch pizzas from backend

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -2,12 +2,43 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { IPizza } from '../../interfaces/IPizza';
 
-export const fetchPizza = createAsyncThunk(
+export const fetchPizzas = createAsyncThunk(
 	'pizzas/fetchAll',
 	async (_, thunkAPI) => {
 		try {
-			const response = await axios.get<IPizza[]>('');
-			return response.data;
+			const response = await axios.get<IPizza[]>(
+				'http://localhost:5000/api/pizzas'
+			);
+
+			const allPizzaTypes = response.data.map((p) => p.types).flat();
+			const sortedTypes: string[] = [];
+			allPizzaTypes.forEach((p) => {
+				if (!sortedTypes.includes(p)) {
+					sortedTypes.push(p);
+				}
+			});
+
+			const checkForSavedPizzas = (): IPizza[] => {
+				if (localStorage.pizzas) {
+					return response.data.map((fetchPizza) => {
+						const replacementPizza: IPizza = JSON.parse(
+							localStorage.pizzas
+						).find(
+							(pizza: IPizza) => fetchPizza.title === pizza.title
+						);
+						if (replacementPizza) {
+							return replacementPizza;
+						}
+						return fetchPizza;
+					});
+				}
+				return response.data;
+			};
+
+			return {
+				pizzas: checkForSavedPizzas(),
+				types: sortedTypes,
+			};
 		} catch (error) {
 			if (error instanceof Error) {
 				return thunkAPI.rejectWithValue(
