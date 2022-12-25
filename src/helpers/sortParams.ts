b08b7d06880a0ea7481a58868dd9b986d331834d
Igor Raskatov <aura.star@yandex.ru>
2022-12-25 17:14:52 +0300

    Add sortParams helper

---
+++ src/helpers/sortParams.ts
@@ -0,0 +1,9 @@
+import { TypeParams } from '../interfaces/TypeParams';
+
+const sortParams: TypeParams[] = [
+	{ param: 'rating', title: 'популярности' },
+	{ param: 'currentPrice', title: 'цене' },
+	{ param: 'title', title: 'алфавиту' },
+];
+
+export default sortParams;
