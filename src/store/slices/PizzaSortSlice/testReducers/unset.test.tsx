b86f16ebe7438affc857c22bad820c920cfae0b2
Igor Raskatov <aura.star@yandex.ru>
2022-12-25 03:33:48 +0300

    Add tests for unset reducer

---
+++ src/store/slices/PizzaSortSlice/testReducers/unset.test.tsx
@@ -0,0 +1,94 @@
+import { IPizza } from '../../../../interfaces/IPizza';
+import pizzasSortReducer, { IUserState, unset } from '../PizzaSortSlice';
+
+const startedPizzas: IPizza[] = [
+	{
+		currentPrice: 100,
+		image: 'http://google.com/image1',
+		title: 'Б',
+		rating: 3,
+		types: ['мясная'],
+		possibleDoughs: ['тонкое'],
+		sizesAndPrices: [
+			{
+				size: '26',
+				price: 100,
+			},
+		],
+	},
+	{
+		currentPrice: 300,
+		image: 'http://google.com/image1',
+		title: 'А',
+		rating: 1,
+		types: ['рыбная'],
+		possibleDoughs: ['тонкое'],
+		sizesAndPrices: [
+			{
+				size: '26',
+				price: 300,
+			},
+		],
+	},
+	{
+		currentPrice: 200,
+		image: 'http://google.com/image2',
+		title: 'В',
+		rating: 5,
+		types: ['мясная', 'сырная'],
+		possibleDoughs: ['тонкое', 'традиционное'],
+		sizesAndPrices: [
+			{
+				size: '26',
+				price: 200,
+			},
+			{
+				size: '30',
+				price: 400,
+			},
+		],
+	},
+];
+
+const initialState: IUserState = {
+	isLoading: false,
+	error: false,
+	pizzas: [],
+	pizzasBackup: startedPizzas,
+	allPizzaTypes: ['тестовая'],
+	currentType: 'все',
+	searchValue: '',
+	isSortedToDown: false,
+	currentSortParam: {
+		title: 'популярности',
+		param: 'rating',
+	},
+};
+
+describe('Reset current type and main pizzas, drop seacrh value', () => {
+	it('Unset', () => {
+		const changedState = pizzasSortReducer(
+			{ ...initialState, searchValue: 'test', currentType: 'мясная' },
+			unset()
+		);
+		expect(changedState.pizzas).toStrictEqual(startedPizzas);
+		expect(changedState.searchValue).toBe('');
+		expect(changedState.currentType).toBe('все');
+	});
+
+	it('With empty pizzasBackup', () => {
+		const changedState = pizzasSortReducer(
+			{
+				...initialState,
+				searchValue: 'test text',
+				currentType: 'сырная',
+				pizzasBackup: [],
+				pizzas: startedPizzas,
+			},
+			unset()
+		);
+		expect(changedState.pizzas).toStrictEqual([]);
+		expect(changedState.searchValue).toBe('');
+		expect(changedState.currentType).toBe('все');
+	});
+});
