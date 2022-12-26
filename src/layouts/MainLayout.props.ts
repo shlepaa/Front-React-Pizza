3f471a87727ab2368128d2ea501d65aea103894b
Igor Raskatov <aura.star@yandex.ru>
2022-12-27 00:16:15 +0300

    Add layout MainLayout

---
+++ src/layouts/MainLayout.props.ts
@@ -0,0 +1,4 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export interface MainLayoutProps
+	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from '../components';
+import styles from './MainLayout.module.scss';
+import cn from 'classnames';
+import { MainLayoutProps } from './MainLayout.props';
+
+export const MainLayout: FC<MainLayoutProps> = ({ className, ...props }) => {
+	return (
+		<div className={cn(styles.wrapper, className)} {...props}>
+			<Header />
+			<Outlet />
+		</div>
+	);
+};
