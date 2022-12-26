e5a632d4335021c9ab69d4635bb42cc3b7ba0774
Igor Raskatov <aura.star@yandex.ru>
2022-12-27 01:57:25 +0300

    Add router wrapper for setRender helper

--- src/helpers/setRender.tsx
+++ src/helpers/setRender.tsx
@@ -1,11 +1,14 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import { createReduxStore } from '../store/store';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const setRender = <T,>(Component: JSX.Element, store?: Record<string, T>) => {
 	return render(
-		<Provider store={createReduxStore(store)}>{Component}</Provider>
+		<MemoryRouter>
+			<Provider store={createReduxStore(store)}>{Component}</Provider>
+		</MemoryRouter>
 	);
 };
 
