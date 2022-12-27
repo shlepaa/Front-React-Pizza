94f39c6d225fb1789f7c6bd031bb4f025e161d79
Igor Raskatov <aura.star@yandex.ru>
2022-12-28 01:46:01 +0300

    Add OrderPosition component

--- src/components/OrderPosition/OrderPosition.tsx
+++ src/components/OrderPosition/OrderPosition.tsx
@@ -15,18 +15,32 @@ export const OrderPosition: FC<OrderPositionProps> = ({
 	...props
 }) => {
 	const [count, setCount] = useState<number>(defaultCount);
+	const [isClosed, setIsClosed] = useState<boolean>(false);
+
+	if (isClosed || count < 1) {
+		return <></>;
+	}
 
 	return (
 		<div className={cn(className, styles.orderPosition)} {...props}>
 			<img className={cn(styles.image)} src={image} alt={title} />
-			<h2>{title}</h2>
-			<span>
-				{dough} тесто, {size} см
-			</span>
-			<CircleButton onClick={() => setCount(count + 1)} />
-			<CircleButton onClick={() => setCount(count - 1)} />
-			<span>{count}</span>
-			<span>{price}</span>
+			<div>
+				<h2 className={styles.title}>{title}</h2>
+				<span className={styles.params}>
+					{dough} тесто, {size} см
+				</span>
+			</div>
+			<div className={styles.countBlock}>
+				<CircleButton isIncrement onClick={() => setCount(count + 1)} />
+				<span className={styles.count}>{count}</span>
+				<CircleButton onClick={() => setCount(count - 1)} />
+			</div>
+			<span className={styles.price}>{price} ₽</span>
+			<CircleButton
+				onClick={() => setIsClosed(true)}
+				className={styles.close}
+				close
+			/>
 		</div>
 	);
 };
